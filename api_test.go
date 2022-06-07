11d1b9e4c07928fd1a2673d15325becc34c55208
thisisaaronland <aaron@localhost>
2022-06-07 14:15:35 -0700

    update vendor deps; move tests inline

--- api_test.go
+++ api_test.go
@@ -1,22 +1,28 @@
-package tests
+package reader
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"github.com/whosonfirst/go-reader"
-	_ "github.com/whosonfirst/go-reader-github"
 	"io"
 	"io/ioutil"
 	"testing"
 )
 
-func TestHTTPReader(t *testing.T) {
+var access_token = flag.String("access-token", "", "A valid GitHub Oauth2 access token")
+
+func TestAPIReader(t *testing.T) {
+
+	if *access_token == "" {
+		return
+	}
 
 	owner := "whosonfirst-data"
 	repo := "whosonfirst-data-admin-ca"
 	branch := "master" // pending rollover (20210114/thisisaaronland)
 
-	reader_uri := fmt.Sprintf("github://%s/%s?branch=%s", owner, repo, branch)
+	reader_uri := fmt.Sprintf("githubapi://%s/%s/%s?access_token=%s", owner, repo, branch, *access_token)
 	file_uri := "101/736/545/101736545.geojson"
 
 	ctx := context.Background()
--- http_test.go
+++ http_test.go
@@ -1,29 +1,21 @@
-package tests
+package reader
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"github.com/whosonfirst/go-reader"
-	_ "github.com/whosonfirst/go-reader-github"
 	"io"
 	"io/ioutil"
 	"testing"
 )
 
-var access_token = flag.String("access-token", "", "A valid GitHub Oauth2 access token")
-
-func TestAPIReader(t *testing.T) {
-
-	if *access_token == "" {
-		return
-	}
+func TestHTTPReader(t *testing.T) {
 
 	owner := "whosonfirst-data"
 	repo := "whosonfirst-data-admin-ca"
 	branch := "master" // pending rollover (20210114/thisisaaronland)
 
-	reader_uri := fmt.Sprintf("githubapi://%s/%s/%s?access_token=%s", owner, repo, branch, *access_token)
+	reader_uri := fmt.Sprintf("github://%s/%s?branch=%s", owner, repo, branch)
 	file_uri := "101/736/545/101736545.geojson"
 
 	ctx := context.Background()
